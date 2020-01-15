f1bbe6adae1194dd666d4af79670d4ea268b7abb
gogotea_milk <gogotea_milk@twitter>
2020-01-16 00:15:47 +0900

    Updates

---
+++ precache-manifest.4b9e8332997a1a1171f491b9e8d1fa67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8716d37003fb31b6d37b991d73f905",
+    "url": "/megido-gacha-site/index.html"
+  },
+  {
+    "revision": "57c17ed851bd2ac7df49",
+    "url": "/megido-gacha-site/static/css/main.3120f1d0.chunk.css"
+  },
+  {
+    "revision": "3f9752eba432d53bd857",
+    "url": "/megido-gacha-site/static/js/2.93ad1ffd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/megido-gacha-site/static/js/2.93ad1ffd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57c17ed851bd2ac7df49",
+    "url": "/megido-gacha-site/static/js/main.f0d55d8d.chunk.js"
+  },
+  {
+    "revision": "259883be04cf0c5e0b2a",
+    "url": "/megido-gacha-site/static/js/runtime-main.cd503bcd.js"
+  }
+]);
