73ee479d65d4b1f936265f1f195504388e48b1f3
gogotea_milk <gogotea_milk@twitter>
2020-01-24 22:59:42 +0900

    - Fix duplicated loop

--- src/megido_list.js
+++ src/megido_list.js
@@ -29,51 +29,46 @@ class MegidoList extends React.Component {
   }
 
   render() {
-    return <table class="table-auto mt-6 border-collapse border-2 border-gray-500">
-      <thead>
-        <tr>
-          <th class="border p-1 bg-green-100">召還済</th>
-          <th class="border p-1 bg-green-100">名前</th>
-          {this.renderGachaListHeader()}
-        </tr>
-      </thead>
-      <tbody>
-        {
-          this.props.megido_chars.map((megido, i) => {
-            return this.props.megido_chars.map((megido, i) => {
-              return (
-                <tr key={megido.id}>
-                  <td class="border text-center">
-                    <input
-                      type="checkbox"
-                      checked={this.props.megido_exist_list.includes(megido.id) }
-                      onChange={() => {
-                        this.props.onChange(megido, i);
-                      }}
-                    />
-                  </td>
-                  <td class="border px-1">
-                    {megido.name +
-                      (megido.regenerated ? short_style[megido.style] : "")}
-                  </td>
-                  {
-                    this.props.megido_gacha_rates[i].map((value, index) => {
-                      return (
-                        <td class="border text-right pr-1" key={index}>
-                          {to_percent(value)}
-                        </td>
-                      );
-                    })
-                  }
+    return (
+      <table class="table-auto mt-6 border-collapse border-2 border-gray-500">
+        <thead>
+          <tr>
+            <th class="border p-1 bg-green-100">召還済</th>
+            <th class="border p-1 bg-green-100">名前</th>
+            {this.renderGachaListHeader()}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.megido_chars.map((megido, i) => {
+            return (
+              <tr key={megido.id}>
+                <td class="border text-center">
+                  <input
+                    type="checkbox"
+                    checked={this.props.megido_exist_list.includes(megido.id)}
+                    onChange={() => {
+                      this.props.onChange(megido, i);
+                    }}
+                  />
+                </td>
+                <td class="border px-1">
+                  {megido.name +
+                    (megido.regenerated ? short_style[megido.style] : "")}
+                </td>
+                {this.props.megido_gacha_rates[i].map((value, index) => {
+                  return (
+                    <td class="border text-right pr-1" key={index}>
+                      {to_percent(value)}
+                    </td>
+                  );
+                })}
               </tr>
             );
-                })
-              }
-          )
-            }
-      </tbody>
-    </table>
-  }  
+          })}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default MegidoList;
